9d945bf913a83aff17bf25312a3e35d415427087
Charles Swaleh <charlswall@live.com>
2023-08-17 16:50:57 +0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-6xl text-rose-700 text-bold font-bold text-center mt-10">Hello World!</h1>
   );
 }
 
